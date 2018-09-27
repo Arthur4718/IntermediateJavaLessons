ca592fe0500a01a8c1a044f8be5107d730adcdf1
Arthur <arthur.gomes_4718@hotmail.com>
2018-09-26 23:48:28 -0300

    Method overload and abstract constructors. Getter and Setter methods to showcase visibility with private and public operators.

--- javapratice/src/main/java/com/interjava/javapratice/Car.java
+++ javapratice/src/main/java/com/interjava/javapratice/Car.java
@@ -3,11 +3,38 @@ package com.interjava.javapratice;
 public class Car {
 
     public String colour = "Black";
+    private int numberOfSeats = 5; // It is wise to use private variables
+    private String mInterior; // Since its private we need a get or set method
 
     public Car(){
         //Constructor Method
         //Additional constructions to construct the object
+        mInterior = "Leather";
     }
 
+    public Car(String chosenColour, String chosenInterior){
+        //Init the object with a set of properties
 
+        this.colour = chosenColour;
+        this.mInterior = chosenInterior;
+    }
+
+    public int getNumberOfSeats() {
+        //add code here
+        //if other classes want to read the value of sets, the get method would give it to them
+        return numberOfSeats;
+    }
+
+    public String getInterior() {
+        return mInterior;
+    }
+
+    public void drive(){
+        System.out.println("Car is moving");
+    }
+
+    public String drive(double milesPerHour){
+        return "Car is Moving";
+
+    }
 }
--- javapratice/src/main/java/com/interjava/javapratice/CarSimulator.java
+++ javapratice/src/main/java/com/interjava/javapratice/CarSimulator.java
@@ -6,5 +6,20 @@ public class CarSimulator {
 
         System.out.println("Launching car simulator");
 
+        Car myFusquinha = new Car();
+
+        System.out.println("The car colour is " + myFusquinha.colour);
+
+        myFusquinha.colour = "Blue";
+
+        System.out.println("The car colour is " + myFusquinha.colour);
+
+        System.out.println("my car has " + myFusquinha.getNumberOfSeats() + " seats");
+
+        Car myHonda = new Car();
+
+        System.out.println("My Honda has " + myHonda.colour + " colour and its interior is " + myHonda.getInterior());
+
+        myHonda.drive();
     }
 }
