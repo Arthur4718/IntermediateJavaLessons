31b0f0c75f7bbf5228f22f131429a47fb51f787d
Arthur <arthur.gomes_4718@hotmail.com>
2018-09-26 22:56:28 -0300

    Car Class and contructor method, along with some changes in VCS.

---
+++ javapratice/src/main/java/com/interjava/javapratice/Car.java
@@ -0,0 +1,13 @@
+package com.interjava.javapratice;
+
+public class Car {
+
+    public String colour = "Black";
+
+    public Car(){
+        //Constructor Method
+        //Additional constructions to construct the object
+    }
+
+
+}
---
+++ javapratice/src/main/java/com/interjava/javapratice/CarSimulator.java
@@ -0,0 +1,10 @@
+package com.interjava.javapratice;
+
+public class CarSimulator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Launching car simulator");
+
+    }
+}
